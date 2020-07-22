650534c41392fafaf7d8c5bcfcffc197d3d5304d
John Caruso <johncaruso@gmail.com>
2020-07-22 13:11:42 -0400

    force wp-url dependency to fix apiFetch dependency on it in some environments (#170)

--- includes/frontend/class-crowdsignal-forms-blocks-assets.php
+++ includes/frontend/class-crowdsignal-forms-blocks-assets.php
@@ -68,7 +68,7 @@ class Crowdsignal_Forms_Blocks_Assets {
 			wp_register_script(
 				$id,
 				$this->url_path( $paths['script'] ),
-				$config['dependencies'],
+				array_merge( array( 'wp-url' ), $config['dependencies'] ), // fix for apiFetch dependency in some environments.
 				$config['version'],
 				true
 			);
