c77f197fae65a243e058234afbc051e8e907eebd
Christian Gastrell <cgastrell@gmail.com>
2020-11-19 15:44:39 -0300

    Release v1.3.3 (#5)
    * Make pot script ignore certain directories
    
    * invert pot/client order, pot creation fails when attempting to extract from built files
    
    * v1.3.3 release preparation

--- scripts/makepot.sh
+++ scripts/makepot.sh
@@ -6,5 +6,4 @@
 
 set -e
 
-docker-compose -f docker/docker-compose.yml exec wordpress bash -c "cd /var/www/html/wp-content/plugins/crowdsignal-forms && wp i18n make-pot --allow-root . languages/crowdsignal-forms.pot"
-
+docker-compose -f docker/docker-compose.yml exec wordpress bash -c "cd /var/www/html/wp-content/plugins/crowdsignal-forms && wp i18n make-pot --allow-root . languages/crowdsignal-forms.pot --exclude={docker,tests,vendor,release,node_modules}"
