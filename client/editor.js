8f143e10039967e4d5f5505dadf6b7279f64e69e
Kuba Birecki <kuba.birecki@crystalplanet.io>
2020-12-11 19:52:15 +0100

    Set-up NPS block framework

--- client/editor.js
+++ client/editor.js
@@ -10,8 +10,10 @@ import pollBlock from 'blocks/poll';
 import voteBlock from 'blocks/vote';
 import voteItemBlock from 'blocks/vote-item';
 import applauseBlock from 'blocks/applause';
+import npsBlock from 'blocks/nps';
 
 registerBlockType( 'crowdsignal-forms/poll', pollBlock );
 registerBlockType( 'crowdsignal-forms/vote', voteBlock );
 registerBlockType( 'crowdsignal-forms/vote-item', voteItemBlock );
 registerBlockType( 'crowdsignal-forms/applause', applauseBlock );
+registerBlockType( 'crowdsignal-forms/nps', npsBlock );
