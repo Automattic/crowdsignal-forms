fcff3ec7d44db8640e4092378cb64a21f8e9bdcc
Christian Gastrell <cgastrell@gmail.com>
2021-02-10 19:41:06 -0300

    add keywords and example, more description

--- client/blocks/nps/index.js
+++ client/blocks/nps/index.js
@@ -11,11 +11,40 @@ import edit from './edit';
 
 export default {
 	title: __( 'NPS', 'crowdsignal-forms' ),
-	description: __( 'Net Promoter Score', 'crowdsignal-forms' ),
+	description: __(
+		'Net Promoter Score popup — powered by Crowdsignal.',
+		'crowdsignal-forms'
+	),
 	category: 'crowdsignal-forms',
 	attributes,
 	supports: {
 		multiple: false,
 	},
 	edit,
+	keywords: [
+		__( 'ask', 'crowdsignal-forms' ),
+		'crowdsignal',
+		__( 'feedback', 'crowdsignal-forms' ),
+		__( 'form', 'crowdsignal-forms' ),
+		__( 'opinion', 'crowdsignal-forms' ),
+		__( 'nps', 'crowdsignal-forms' ),
+		__( 'score', 'crowdsignal-forms' ),
+		__( 'promoter', 'crowdsignal-forms' ),
+		__( 'research', 'crowdsignal-forms' ),
+		__( 'survey', 'crowdsignal-forms' ),
+	],
+	example: {
+		attributes: {
+			ratingQuestion: __(
+				'How satisfied are your with the content of the site?',
+				'crowdsignal-forms'
+			),
+			feedbackQuestion: __(
+				'Any advise on how we could improve your experience?',
+				'crowdsignal-forms'
+			),
+			lowRatingLabel: __( 'Not satisfied', 'crowdsignal-forms' ),
+			highRatingLabel: __( 'Very satisfied', 'crowdsignal-forms' ),
+		},
+	},
 };
