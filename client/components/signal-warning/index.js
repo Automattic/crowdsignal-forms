73be553eb99d28d79a12a5d74c6c8d27b3e19b90
Christian Gastrell <cgastrell@gmail.com>
2021-02-12 18:26:56 -0300

    Use Notice component for SignalWarning (#34)
    * use WP component Notice inside SignalWarning
    
    * Adjust styles for SignalWarning with Notice
    
    * Make styles generic regarding common components (editor-notice)
    
    * Introduce EditorNotice component. Make SignalWarning a wrapper around it.
    
    * Make EditorNotice content grow as much as possible to effectively align buttons to the left

--- client/components/signal-warning/index.js
+++ client/components/signal-warning/index.js
@@ -7,30 +7,30 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import Icon from 'components/icon/warning-circle';
+import EditorNotice from 'components/editor-notice';
+import WarningIcon from 'components/icon/warning-circle';
 
 const SignalWarning = () => {
 	return (
-		<div className="crowdsignal-forms__signal-warning">
-			<div className="crowdsignal-forms__signal-warning-icon">
-				<Icon />
-			</div>
-			<div className="crowdsignal-forms__signal-warning-message">
-				{ __(
-					'Your free Crowdsignal account has exceeded ',
-					'crowdsignal-forms'
-				) }
-				<ExternalLink href="https://crowdsignal.com/support/what-is-a-signal/">
-					{ __( 'the limit of 2500 signals.', 'crowdsignal-forms' ) }
+		<EditorNotice
+			icon={ <WarningIcon /> }
+			status="warn"
+			isDismissible={ false }
+		>
+			{ __(
+				'Your free Crowdsignal account has exceeded ',
+				'crowdsignal-forms'
+			) }
+			<ExternalLink href="https://crowdsignal.com/support/what-is-a-signal/">
+				{ __( 'the limit of 2500 signals.', 'crowdsignal-forms' ) }
+			</ExternalLink>
+			<br />
+			<strong>
+				<ExternalLink href="https://crowdsignal.com/pricing">
+					{ __( 'Please upgrade.', 'crowdsignal-forms' ) }
 				</ExternalLink>
-				<br />
-				<strong>
-					<ExternalLink href="https://crowdsignal.com/pricing">
-						{ __( 'Please upgrade.', 'crowdsignal-forms' ) }
-					</ExternalLink>
-				</strong>
-			</div>
-		</div>
+			</strong>
+		</EditorNotice>
 	);
 };
 
