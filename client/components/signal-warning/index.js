8c4c74c8e796c37d0cda44d52e17e276671ecebd
Christian Gastrell <cgastrell@gmail.com>
2021-02-12 14:35:44 -0300

    Introduce EditorNotice component. Make SignalWarning a wrapper around it.

--- client/components/signal-warning/index.js
+++ client/components/signal-warning/index.js
@@ -1,40 +1,36 @@
 /**
  * WordPress dependencies
  */
-import { ExternalLink, Notice } from '@wordpress/components';
+import { ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import Icon from 'components/icon/warning-circle';
+import EditorNotice from 'components/editor-notice';
+import WarningIcon from 'components/icon/warning-circle';
 
 const SignalWarning = () => {
 	return (
-		<Notice
-			className="crowdsignal-forms__editor-notice"
+		<EditorNotice
+			icon={ <WarningIcon /> }
 			status="warn"
 			isDismissible={ false }
 		>
-			<div className="crowdsignal-forms__editor-notice-icon">
-				<Icon />
-			</div>
-			<div className="crowdsignal-forms__editor-notice-text">
-				{ __(
-					'Your free Crowdsignal account has exceeded ',
-					'crowdsignal-forms'
-				) }
-				<ExternalLink href="https://crowdsignal.com/support/what-is-a-signal/">
-					{ __( 'the limit of 2500 signals.', 'crowdsignal-forms' ) }
+			{ __(
+				'Your free Crowdsignal account has exceeded ',
+				'crowdsignal-forms'
+			) }
+			<ExternalLink href="https://crowdsignal.com/support/what-is-a-signal/">
+				{ __( 'the limit of 2500 signals.', 'crowdsignal-forms' ) }
+			</ExternalLink>
+			<br />
+			<strong>
+				<ExternalLink href="https://crowdsignal.com/pricing">
+					{ __( 'Please upgrade.', 'crowdsignal-forms' ) }
 				</ExternalLink>
-				<br />
-				<strong>
-					<ExternalLink href="https://crowdsignal.com/pricing">
-						{ __( 'Please upgrade.', 'crowdsignal-forms' ) }
-					</ExternalLink>
-				</strong>
-			</div>
-		</Notice>
+			</strong>
+		</EditorNotice>
 	);
 };
 
