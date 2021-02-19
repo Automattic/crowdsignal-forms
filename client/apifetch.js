96d7830becd6fe1424bc0aa4135ee088fd8ee84d
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-02-19 19:07:53 +0100

    Add middlewares for configuring the request similar to wp.apiFetch

--- client/apifetch.js
+++ client/apifetch.js
@@ -41,4 +41,52 @@ const apiFetch = async ( options ) => run( options, middlewareRegistry );
  */
 apiFetch.use = ( middleware ) => middlewareRegistry.push( middleware );
 
+/**
+ * Prefix the request URLs with '/wp-json'
+ */
+apiFetch.use( ( options, next ) => {
+	if ( options.path.indexOf( '/crowdsignal-forms/v1' ) === 0 ) {
+		options.path = `/wp-json${ options.path }`;
+	}
+
+	return next( options );
+} );
+
+/**
+ * Set default headers
+ */
+apiFetch.use( ( options, next ) => {
+	const headers = options.headers || {};
+
+	return next( {
+		...options,
+		headers: {
+			...headers,
+			// The backend uses the Accept header as a condition for considering an
+			// incoming request as a REST request.
+			//
+			// See: https://core.trac.wordpress.org/ticket/44534
+			Accept: 'application/json, */*;q=0.1',
+		},
+	} );
+} );
+
+/**
+ * Convert data to JSON
+ */
+apiFetch.use( ( { data, ...options }, next ) => {
+	if ( ! data ) {
+		return next( options );
+	}
+
+	return next( {
+		...options,
+		headers: {
+			...options.headers,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify( data ),
+	} );
+} );
+
 export default apiFetch;
