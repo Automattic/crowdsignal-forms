a2029936a4a2b385c1363477e037aa5534b12995
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-03-24 21:20:29 +0100

    Add block skeleton for the feedback block

---
+++ client/blocks/feedback/attributes.js
@@ -0,0 +1,20 @@
+/**
+ * Note: Any changes made to the attributes definition need to be duplicated in
+ *       Crowdsignal_Forms\Frontend\Blocks\Crowdsignal_Forms_Nps_Block::attributes()
+ *       inside includes/frontend/blocks/class-crowdsignal-forms-nps-block.php.
+ */
+
+export default {
+	hideBranding: {
+		type: 'boolean',
+		default: false,
+	},
+	surveyId: {
+		type: 'number',
+		default: null,
+	},
+	title: {
+		type: 'string',
+		default: '',
+	},
+};
---
+++ client/blocks/feedback/edit.js
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * WordPress depenencies
+ */
+import { compose } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import ConnectToCrowdsignal from 'components/connect-to-crowdsignal';
+import { withFallbackStyles } from 'components/with-fallback-styles';
+
+const EditFeedbackBlock = ( props ) => {
+	return (
+		<ConnectToCrowdsignal>
+			Hello! One feedback please.
+
+			{ props.renderStyleProbe() }
+		</ConnectToCrowdsignal>
+	);
+};
+
+export default compose( [
+	withFallbackStyles,
+] )( EditFeedbackBlock );
