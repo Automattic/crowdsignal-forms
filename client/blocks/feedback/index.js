9f5a1d20fc4013267733c730e5d7b7b98f5bb1ba
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-03-24 21:20:29 +0100

    Add block skeleton for the feedback block

---
+++ client/blocks/feedback/index.js
@@ -0,0 +1,24 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import PollIcon from 'components/icon/poll';
+import attributes from './attributes';
+import EditFeedbackBlock from './edit';
+
+export default {
+	title: __( 'Feedback', 'crowdsignal-forms' ),
+	description: __( 'Feedback block', 'crowdsignal-forms' ),
+	category: 'crowdsignal-forms',
+	keywords: [
+		'crowdsignal',
+		__( 'feedback', 'crowdsignal-forms' ),
+	],
+	icon: <PollIcon />,
+	edit: EditFeedbackBlock,
+	attributes,
+};
--- client/editor.js
+++ client/editor.js
@@ -11,9 +11,11 @@ import voteBlock from 'blocks/vote';
 import voteItemBlock from 'blocks/vote-item';
 import applauseBlock from 'blocks/applause';
 import npsBlock from 'blocks/nps';
+import feedbackBlock from 'blocks/feedback';
 
 registerBlockType( 'crowdsignal-forms/poll', pollBlock );
 registerBlockType( 'crowdsignal-forms/vote', voteBlock );
 registerBlockType( 'crowdsignal-forms/vote-item', voteItemBlock );
 registerBlockType( 'crowdsignal-forms/applause', applauseBlock );
 registerBlockType( 'crowdsignal-forms/nps', npsBlock );
+registerBlockType( 'crowdsignal-forms/feedback', feedbackBlock );
