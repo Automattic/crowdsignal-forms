1e5d72b9f349aaf5cdd034a5e4cbea8750f8faa8
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-04-12 20:48:12 +0200

    Add feedback block icons

---
+++ client/components/icon/feedback.js
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export default () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M18.5 4.5H4.5C4.22386 4.5 4 4.72386 4 5V19.5L6.70711 16.7929C6.89464 16.6054 7.149 16.5 7.41421 16.5H18.5C19.0523 16.5 19.5 16.0523 19.5 15.5V5.5C19.5 4.94772 19.0523 4.5 18.5 4.5Z"
+			stroke="black"
+			strokeWidth="1.5"
+			fill="none"
+		/>
+		<rect x="7" y="9.5" width="1.5" height="1.5" rx="0.75" fill="black" />
+		<rect x="11" y="9.5" width="1.5" height="1.5" rx="0.75" fill="black" />
+		<rect x="15" y="9.5" width="1.5" height="1.5" rx="0.75" fill="black" />
+	</svg>
+);
---
+++ client/components/icon/placement.js
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export const TopLeftPlacementIcon = () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="3.75" y="3.75" width="3" height="3" fill="black" />
+		<rect x="18" y="4.5" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="18" width="1.5" height="1.5" fill="black" />
+		<rect x="4.5" y="18" width="1.5" height="1.5" fill="black" />
+	</svg>
+);
+
+export const TopRightPlacementIcon = () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="4.5" y="4.5" width="1.5" height="1.5" fill="black" />
+		<rect x="17.25" y="3.75" width="3" height="3" fill="black" />
+		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="18" width="1.5" height="1.5" fill="black" />
+		<rect x="4.5" y="18" width="1.5" height="1.5" fill="black" />
+	</svg>
+);
+
+export const BottomLeftPlacementIcon = () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="4.5" y="4.5" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="4.5" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="18" width="1.5" height="1.5" fill="black" />
+		<rect x="3.75" y="17.25" width="3" height="3" fill="black" />
+	</svg>
+);
+
+export const BottomRightPlacementIcon = () => (
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="4.5" y="4.5" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="4.5" width="1.5" height="1.5" fill="black" />
+		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
+		<rect x="17.25" y="17.25" width="3" height="3" fill="black" />
+		<rect x="4.5" y="18" width="1.5" height="1.5" fill="black" />
+	</svg>
+);
