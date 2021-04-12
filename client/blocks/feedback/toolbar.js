cc8814724f9341d96cf5aa657fc46006cf96680b
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-04-12 20:48:32 +0200

    Update feedback block icons

--- client/blocks/feedback/toolbar.js
+++ client/blocks/feedback/toolbar.js
@@ -9,65 +9,83 @@ import React, { useState } from 'react';
 import { BlockControls } from '@wordpress/block-editor';
 import {
 	Button,
+	Icon,
 	Popover,
 	ToolbarButton,
 	ToolbarGroup,
 } from '@wordpress/components';
 
-const FeedbackToolbar = ( { onChangePosition } ) => {
+/**
+ * Internal dependencies
+ */
+import {
+	TopLeftPlacementIcon,
+	TopRightPlacementIcon,
+	BottomLeftPlacementIcon,
+	BottomRightPlacementIcon,
+} from 'components/icon/placement';
+
+const placementIcons = {
+	'top-left': TopLeftPlacementIcon,
+	'top-right': TopRightPlacementIcon,
+	'bottom-left': BottomLeftPlacementIcon,
+	'bottom-right': BottomRightPlacementIcon,
+};
+
+const FeedbackToolbar = ( { attributes, onChangePosition } ) => {
 	const [ showPosition, setShowPosition ] = useState( false );
 
 	const showPositionPopover = () => setShowPosition( true );
 	const hidePositionPopover = () => setShowPosition( false );
 
+	const { x, y } = attributes;
+
 	return (
 		<BlockControls>
 			<ToolbarGroup>
-				<ToolbarButton onClick={ showPositionPopover }>
+				<ToolbarButton
+					onClick={ showPositionPopover }
+					icon={ placementIcons[ `${ y }-${ x }` ] }
+				>
 					{ showPosition && (
-						<Popover onClose={ hidePositionPopover }>
-							<Button
-								onClick={ () =>
-									onChangePosition( 'left', 'top' )
-								}
-							>
-								Top Left
-							</Button>
-							<Button
-								onClick={ () =>
-									onChangePosition( 'left', 'center' )
-								}
-							>
-								Center Left
-							</Button>
-							<Button
-								onClick={ () =>
-									onChangePosition( 'left', 'bottom' )
-								}
-							>
-								Bottom left
-							</Button>
-							<Button
-								onClick={ () =>
-									onChangePosition( 'right', 'top' )
-								}
-							>
-								Top right
-							</Button>
-							<Button
-								onClick={ () =>
-									onChangePosition( 'right', 'center' )
-								}
-							>
-								Center right
-							</Button>
-							<Button
-								onClick={ () =>
-									onChangePosition( 'right', 'bottom' )
-								}
-							>
-								Bottom right
-							</Button>
+						<Popover
+							className="crowdsignal-forms-feedback__toolbar-popover-wrapper"
+							onClose={ hidePositionPopover }
+						>
+							<div className="crowdsignal-forms-feedback__toolbar-popover">
+								<Button
+									className="crowdsignal-forms-feedback__position-button"
+									onClick={ () =>
+										onChangePosition( 'left', 'top' )
+									}
+								>
+									<Icon icon={ TopLeftPlacementIcon } />
+								</Button>
+								<Button
+									className="crowdsignal-forms-feedback__position-button"
+									onClick={ () =>
+										onChangePosition( 'right', 'top' )
+									}
+								>
+									<Icon icon={ TopRightPlacementIcon } />
+								</Button>
+								<Button
+									className="crowdsignal-forms-feedback__position-button"
+									onClick={ () =>
+										onChangePosition( 'left', 'bottom' )
+									}
+								>
+									<Icon icon={ BottomLeftPlacementIcon } />
+								</Button>
+								<Button
+									className="crowdsignal-forms-feedback__position-button"
+									onClick={ () =>
+										onChangePosition( 'right', 'bottom' )
+									}
+								>
+									<Icon icon={ BottomRightPlacementIcon } />
+								</Button>
+							</div>
 						</Popover>
 					) }
 				</ToolbarButton>
