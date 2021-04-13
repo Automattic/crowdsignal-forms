49c6f697ae4d8d6dc9b5ce0c422d0ee6ff448d79
Christian Gastrell <cgastrell@gmail.com>
2021-04-13 18:52:32 -0300

    Turn SignalNotice second link to an action (button)

--- client/components/signal-warning/index.js
+++ client/components/signal-warning/index.js
@@ -11,7 +11,19 @@ import EditorNotice from 'components/editor-notice';
 
 const SignalWarning = () => {
 	return (
-		<EditorNotice icon="warning" status="warn" isDismissible={ false }>
+		<EditorNotice
+			icon="warning"
+			status="warn"
+			isDismissible={ false }
+			actions={ [
+				{
+					label: __( 'Please upgrade', 'crowdsignal-forms' ),
+					url: 'https://crowdsignal.com/pricing',
+					className: 'is-secondary',
+					noDefaultClasses: true,
+				},
+			] }
+		>
 			{ __(
 				'Your free Crowdsignal account has exceeded ',
 				'crowdsignal-forms'
@@ -19,12 +31,6 @@ const SignalWarning = () => {
 			<ExternalLink href="https://crowdsignal.com/support/what-is-a-signal/">
 				{ __( 'the limit of 2500 signals.', 'crowdsignal-forms' ) }
 			</ExternalLink>
-			<br />
-			<strong>
-				<ExternalLink href="https://crowdsignal.com/pricing">
-					{ __( 'Please upgrade.', 'crowdsignal-forms' ) }
-				</ExternalLink>
-			</strong>
 		</EditorNotice>
 	);
 };
