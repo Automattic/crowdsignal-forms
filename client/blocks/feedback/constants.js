74b00bad2f8f81713a1f783d451cbaa33c95c3d6
Christian Gastrell <cgastrell@gmail.com>
2021-04-15 17:43:09 -0300

    Add status constants and attributes

--- client/blocks/feedback/constants.js
+++ client/blocks/feedback/constants.js
@@ -2,3 +2,9 @@ export const views = {
 	QUESTION: 'question',
 	SUBMIT: 'submit',
 };
+
+export const FeedbackStatus = Object.freeze( {
+	OPEN: 'open',
+	CLOSED: 'closed',
+	CLOSED_AFTER: 'closed-after',
+} );
