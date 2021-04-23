89864681553cb55529f2d32e70b73d9b5827f0f5
Kuba Birecki <ice9js@users.noreply.github.com>
2021-04-23 21:08:25 +0200

    Update placement toolbar icon based on block position (#94)
    * Update placement toolbar icon based on block position
    
    * Fix linter errors

--- client/components/icon/placement.js
+++ client/components/icon/placement.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 
-export const TopLeftPlacementIcon = () => (
+const PlacementIcon = () => (
 	<svg
 		width="24"
 		height="24"
@@ -11,62 +11,11 @@ export const TopLeftPlacementIcon = () => (
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
 		<rect x="3.75" y="3.75" width="3" height="3" fill="black" />
 		<rect x="18" y="4.5" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
 		<rect x="18" y="18" width="1.5" height="1.5" fill="black" />
 		<rect x="4.5" y="18" width="1.5" height="1.5" fill="black" />
 	</svg>
 );
 
-export const TopRightPlacementIcon = () => (
-	<svg
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
-		<rect x="4.5" y="4.5" width="1.5" height="1.5" fill="black" />
-		<rect x="17.25" y="3.75" width="3" height="3" fill="black" />
-		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="18" width="1.5" height="1.5" fill="black" />
-		<rect x="4.5" y="18" width="1.5" height="1.5" fill="black" />
-	</svg>
-);
-
-export const BottomLeftPlacementIcon = () => (
-	<svg
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
-		<rect x="4.5" y="4.5" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="4.5" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="18" width="1.5" height="1.5" fill="black" />
-		<rect x="3.75" y="17.25" width="3" height="3" fill="black" />
-	</svg>
-);
-
-export const BottomRightPlacementIcon = () => (
-	<svg
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<rect x="4.5" y="11.25" width="1.5" height="1.5" fill="black" />
-		<rect x="4.5" y="4.5" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="4.5" width="1.5" height="1.5" fill="black" />
-		<rect x="18" y="11.25" width="1.5" height="1.5" fill="black" />
-		<rect x="17.25" y="17.25" width="3" height="3" fill="black" />
-		<rect x="4.5" y="18" width="1.5" height="1.5" fill="black" />
-	</svg>
-);
+export default PlacementIcon;
