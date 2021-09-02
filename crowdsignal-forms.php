08b2ad71f868ac02a63d241568defc2c169a144a
Christian Gastrell <cgastrell@gmail.com>
2021-09-02 11:28:05 -0300

    Release v1.5.7 bump (#171)

--- crowdsignal-forms.php
+++ crowdsignal-forms.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Crowdsignal Forms
  * Plugin URI:        https://crowdsignal.com/crowdsignal-forms/
  * Description:       Crowdsignal Form Blocks
- * Version:           1.5.6
+ * Version:           1.5.7
  * Author:            Automattic
  * Author URI:        https://automattic.com/
  * License:           GPL-2.0+
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-define( 'CROWDSIGNAL_FORMS_VERSION', '1.5.6' );
+define( 'CROWDSIGNAL_FORMS_VERSION', '1.5.7' );
 define( 'CROWDSIGNAL_FORMS_PLUGIN_FILE', __FILE__ );
 define( 'CROWDSIGNAL_FORMS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
