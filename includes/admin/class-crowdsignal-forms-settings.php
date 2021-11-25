df767a60e25760fd4c1e534951deaf29d1b64807
Donncha Ó Caoimh <5656673+donnchawp@users.noreply.github.com>
2021-11-25 10:08:59 +0000

    WIP: Move the "Getting Started" process into the Settings page. (#179)
    Not ready yet - needs changes to text and review/update of process.

--- includes/admin/class-crowdsignal-forms-settings.php
+++ includes/admin/class-crowdsignal-forms-settings.php
@@ -8,7 +8,10 @@
 
 namespace Crowdsignal_Forms\Admin;
 
+use Crowdsignal_Forms\Admin\Crowdsignal_Forms_Admin_Notices;
+use Crowdsignal_Forms\Admin\Crowdsignal_Forms_Notice_Icon;
 use Crowdsignal_Forms\Auth\Crowdsignal_Forms_Api_Authenticator;
+use Crowdsignal_Forms\Crowdsignal_Forms;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -20,6 +23,13 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 0.9.0
  */
 class Crowdsignal_Forms_Settings {
+
+	/**
+	 * The step in the "Getting Started Process".
+	 *
+	 * @var int step.
+	 */
+	private $step = false;
 
 	/**
 	 * Our Settings.
@@ -36,13 +46,51 @@ class Crowdsignal_Forms_Settings {
 		add_action( 'admin_init', array( $this, 'update_settings' ) );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 12 );
+
+		add_filter( 'crowdsignal_forms_show_admin_notice_' . Crowdsignal_Forms_Admin_Notices::NOTICE_CORE_SETUP, array( $this, 'show_setup_notice' ) );
+		add_filter( 'crowdsignal_forms_show_admin_notice_' . Crowdsignal_Forms_Admin_Notices::SETUP_SUCCESS, array( $this, 'show_setup_success' ) );
+		Crowdsignal_Forms_Admin_Notices::add_notice( Crowdsignal_Forms_Admin_Notices::SETUP_SUCCESS );
 	}
 
 	/**
 	 * Enqueues scripts for setup page.
 	 */
 	public function admin_enqueue_scripts() {
+		wp_enqueue_style( 'jetpack-styles1', 'https://c0.wp.com/p/jetpack/10.0/_inc/build/style.min.css', array(), '1.5.7' );
+		wp_enqueue_style( 'jetpack-styles2', 'https://c0.wp.com/c/5.8.1/wp-admin/css/common.min.css', array(), '1.5.7' );
+		wp_enqueue_style( 'jetpack-styles3', 'https://c0.wp.com/p/jetpack/10.0/_inc/build/admin.css', array(), '1.5.7' );
 		wp_enqueue_style( 'admin-styles', plugin_dir_url( __FILE__ ) . '/admin-styles.css', array(), '1.5.12' );
+		wp_enqueue_script( 'videopress', 'https://videopress.com/videopress-iframe.js', array(), '1.0', false );		
+	}
+
+	/**
+	 * Convenience method to get the icon markup from the Notice_Icon helper class
+	 *
+	 * @param string $icon The icon type: warning|success.
+	 */
+	public static function get_icon( $icon ) {
+		switch ( $icon ) {
+			case 'warning':
+				return Crowdsignal_Forms_Notice_Icon::warning();
+			case 'success':
+				return Crowdsignal_Forms_Notice_Icon::success();
+			default:
+				return '';
+		}
+	}
+
+	/**
+	 * Hides the "Getting Started" admin notice if the plugin is already connected to Crowdsignal.
+	 *
+	 * @param bool $show to show the notice or not.
+	 */
+	public function show_setup_notice( $show ) {
+		$api_auth_provider = Crowdsignal_Forms::instance()->get_api_authenticator();
+		if ( $api_auth_provider->get_user_code() ) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	/**
@@ -63,6 +111,11 @@ class Crowdsignal_Forms_Settings {
 	 * @access protected
 	 */
 	protected function init_settings() {
+
+		// we're all done, remove the notice.
+		if ( 1 !== $this->step ) {
+			Crowdsignal_Forms_Admin_Notices::remove_notice( Crowdsignal_Forms_Admin_Notices::NOTICE_CORE_SETUP );
+		}
 
 		$this->settings = apply_filters(
 			'crowdsignal_forms_settings',
@@ -110,6 +163,49 @@ class Crowdsignal_Forms_Settings {
 			return;
 		}
 
+		$api_auth_provider = new Crowdsignal_Forms_Api_Authenticator();
+
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- got_api_key check later
+		$this->step = ! empty( $_GET['step'] ) ? absint( $_GET['step'] ) : 1;
+		if ( isset( $_SERVER['REQUEST_METHOD'] ) && 'POST' === $_SERVER['REQUEST_METHOD'] ) {
+			if ( 2 === $this->step && isset( $_POST['got_api_key'] ) && isset( $_POST['api_key'] ) && get_option( 'crowdsignal_api_key_secret' ) === $_POST['got_api_key'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing -- got_api_key
+				$api_key = sanitize_key( wp_unslash( $_POST['api_key'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Missing -- got_api_key
+				$api_auth_provider->set_api_key( $api_key );
+				$api_auth_provider->get_user_code_for_key( $api_key );
+				delete_option( 'crowdsignal_api_key_secret' );
+				include dirname( __FILE__ ) . '/views/html-admin-setup-step-2.php';
+				return;
+			} else {
+				$this->step = 1; // repeat the setup.
+			}
+		} elseif ( 1 === $this->step ) {
+			update_option( 'crowdsignal_api_key_secret', md5( time() . wp_rand() ) );
+
+			$existing_api_key = $api_auth_provider->get_api_key();
+			if ( ! $existing_api_key ) {
+				$existing_api_key = get_option( 'polldaddy_api_key' );
+				$api_auth_provider->set_api_key( $existing_api_key );
+			}
+
+			if ( $existing_api_key ) {
+				$existing_user_code = $api_auth_provider->get_user_code_for_key( $existing_api_key );
+
+				if ( $existing_user_code ) {
+					if ( $api_auth_provider->get_user_code() !== $existing_user_code ) {
+						$api_auth_provider->set_user_code( $existing_user_code );
+					}
+					delete_option( 'crowdsignal_api_key_secret' );
+					$this->step = 3;
+				} else {
+					/**
+					 * Cached API key may have been deleted on the server.
+					 * Force reconnection.
+					 */
+					$api_auth_provider->delete_api_key();
+				}
+			}
+		}
+
 		if (
 			isset( $_POST['action'] ) &&
 			isset( $_POST['crowdsignal_api_key'] ) &&
@@ -121,20 +217,20 @@ class Crowdsignal_Forms_Settings {
 					$api_key = sanitize_key( wp_unslash( $_POST['crowdsignal_api_key'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Missing -- got_api_key
 					if ( ! empty( $api_key ) && $api_auth_provider->get_user_code_for_key( $api_key ) ) {
 						$api_auth_provider->set_api_key( $api_key );
-						wp_safe_redirect( admin_url( 'admin.php?page=crowdsignal-forms-settings&msg=api-key-added' ) );
+						wp_safe_redirect( admin_url( 'options-general.php?page=crowdsignal-forms-settings&msg=api-key-added' ) );
 					} else {
-						wp_safe_redirect( admin_url( 'admin.php?page=crowdsignal-forms-settings&msg=api-key-not-added' ) );
+						wp_safe_redirect( admin_url( 'options-general.php?page=crowdsignal-forms-settings&msg=api-key-not-added' ) );
 					}
 				} else {
-					wp_safe_redirect( admin_url( 'admin.php?page=crowdsignal-forms-settings&msg=bad-nonce' ) );
+					wp_safe_redirect( admin_url( 'options-general.php?page=crowdsignal-forms-settings&msg=bad-nonce' ) );
 				}
 			} elseif ( 'disconnect' === $_POST['action'] ) {
 				if ( ! wp_verify_nonce( sanitize_key( $_POST['_wpnonce'] ), 'disconnect-api-key' ) ) {
-					wp_safe_redirect( admin_url( 'admin.php?page=crowdsignal-forms-settings&msg=disconnect-failed' ) );
+					wp_safe_redirect( admin_url( 'options-general.php?page=crowdsignal-forms-settings&msg=disconnect-failed' ) );
 				} else {
 					$api_auth_provider->delete_api_key();
 					$api_auth_provider->delete_user_code();
-					wp_safe_redirect( admin_url( 'admin.php?page=crowdsignal-forms-settings&msg=disconnected' ) );
+					wp_safe_redirect( admin_url( 'options-general.php?page=crowdsignal-forms-settings&msg=disconnected' ) );
 				}
 			}
 		}
@@ -145,95 +241,21 @@ class Crowdsignal_Forms_Settings {
 	 */
 	public function output() {
 		$this->init_settings();
+
+		$api_auth_provider = new Crowdsignal_Forms_Api_Authenticator();
+		$api_key           = $api_auth_provider->get_api_key();
+		$api_key_name      = get_option( Crowdsignal_Forms_Api_Authenticator::API_KEY_NAME );
+
 		include dirname( __FILE__ ) . '/views/html-admin-setup-header.php';
-		?>
-		<div class="crowdsignal-settings-wrap">
 
-			<div class="crowdsignal-settings__main-content">
-				<div class="dops-card dops-section-header is-compact">
-					<div class="dops-section-header__label">
-						<span class="dops-section-header__label-text">Settings</span>
-					</div>
-				</div>
+		if ( ! $api_key ) {
+			include dirname( __FILE__ ) . '/views/html-admin-setup-step-1.php';
+		} else {
+			include dirname( __FILE__ ) . '/views/html-admin-settings.php';
+			include dirname( __FILE__ ) . '/views/html-admin-setup-step-3.php';
+			include dirname( __FILE__ ) . '/views/html-admin-dashboard-teaser.php';
+		}
 
-				<div class="dops-card dops-section-header is-compact">
-
-					<div class="crowdsignal-settings__middle">
-						<div class="crowdsignal-settings__text">
-							<?php
-								echo wp_kses_post(
-									sprintf(
-										/* translators: %s is a link to the Crowdsignal connection page. */
-										__( 'To collect responses and data with Crowdsignal Forms you need to <a href="%s" target="_blank">connect the plugin with a Crowdsignal account.</a>', 'crowdsignal-forms' ),
-										'/wp-admin/admin.php?page=crowdsignal-forms-setup'
-									)
-								);
-							?>
-							<br>
-							<?php esc_html_e( 'You can do this by entering an API key below:', 'crowdsignal-forms' ); ?>
-						</div>
-
-						<form class="crowdsignal-options" method="post" action="<?php echo esc_url( admin_url( 'admin.php?page=crowdsignal-forms-settings' ) ); ?>">
-							<?php
-							// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Used for basic flow.
-							if ( ! empty( $_GET['settings-updated'] ) ) {
-								echo '<div class="updated fade crowdsignal-updated"><p>' . esc_html__( 'Settings successfully saved', 'crowdsignal-forms' ) . '</p></div>';
-							}
-
-							$api_auth_provider = new Crowdsignal_Forms_Api_Authenticator();
-							?>
-							<div id="settings-general" class="settings_panel">
-								<table class="form-table settings parent-settings">
-									<tr valign="top" class="">
-										<th scope="row"><label for="setting-crowdsignal_api_key"><?php esc_html_e( 'Enter Crowdsignal API Key', 'crowdsignal-forms' ); ?></a></th>
-										<td><input
-											<?php echo $api_auth_provider->get_api_key() ? 'readonly' : ''; ?>
-											id="setting-crowdsignal_api_key"
-											class="regular-text"
-											type="text"
-											name="crowdsignal_api_key"
-											value="<?php echo esc_attr( $api_auth_provider->get_api_key() ); ?>"
-											/>
-										</td>
-									</tr>
-								</table>
-							</div>
-
-							<div class="crowdsignal-settings__submit">
-							<?php
-							if ( $api_auth_provider->get_api_key() ) {
-								wp_nonce_field( 'disconnect-api-key' );
-								?>
-								<input type="hidden" name="action" value="disconnect" />
-								<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Disconnect', 'crowdsignal-forms' ); ?>" />
-								<?php
-							} else {
-								wp_nonce_field( 'add-api-key' );
-								?>
-								<input type="hidden" name="action" value="update" />
-								<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Connect', 'crowdsignal-forms' ); ?>" />
-								<?php
-							}
-							?>
-							</div>
-						</form>
-
-						<div class="crowdsignal-settings__bottom">
-							<div class="crowdsignal-settings__text">
-								<?php
-								if ( ! get_option( Crowdsignal_Forms_Api_Authenticator::API_KEY_NAME ) ) {
-									esc_html_e( "If you don't have an API key we can help you here: ", 'crowdsignal-forms' );
-									echo '<a class="button" rel="noopener noreferrer" href="/wp-admin/admin.php?page=crowdsignal-forms-setup">Get an API Key</a>';
-								}
-								?>
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php
 		include dirname( __FILE__ ) . '/views/html-admin-setup-footer.php';
 	}
 }
---
+++ includes/admin/views/html-admin-settings.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * File containing the view used in the settings page.
+ *
+ * @package Crowdsignal_Forms\Admin
+ */
+
+?>
+<div class='jp-settings-container'>
+	<div class="dops-card dops-section-header is-compact">
+		<div class="dops-section-header__label">
+			<span class="dops-section-header__label-text"><?php esc_html_e( 'API Key', 'crowdsignal-forms' ); ?></span>
+		</div>
+	</div>
+
+	<div class="dops-card dops-section-header is-compact">
+		<div class="jp-form-settings-group">
+			<p>
+			<?php
+			printf(
+				/* translators: Placeholder is the text "Crowdsignal". */
+				esc_html__( 'You need to connect this plugin with a %s account to collect responses and data.', 'crowdsignal-forms' ),
+				'<a href="https://crowdsignal.com/">Crowdsignal</a>'
+			);
+			?>
+			</p>
+			<?php if ( ! $api_key ) { ?>
+				<p><?php esc_html_e( 'If you have a Crowdsignal account, click the "Get API Key" button to connect. This will open a new window.', 'crowdsignal-forms' ); ?>
+				<form id="cs-connect-form" class="crowdsignal-options" method="post" action="https://app.crowdsignal.com/get-api-key/" target="CSCONNECT">
+				<input type="hidden" name="get_api_key" value="<?php echo esc_attr( get_option( 'crowdsignal_api_key_secret' ) ); ?>" />
+				<input type="hidden" name="ref" value="<?php echo esc_attr( admin_url( 'options-general.php?page=crowdsignal-forms-settings' ) ); ?>" />
+				<input type="submit" value="<?php esc_html_e( 'Get API Key', 'crowdsignal-forms' ); ?>" class="dops-button is-primary" />
+				</form></p>
+			<?php } ?>
+			<p><?php esc_html_e( 'If you know your API key you can paste it in to the box below.', 'crowdsignal-forms' ); ?>
+
+			<form class="crowdsignal-options" method="post" action="<?php echo esc_url( admin_url( 'options-general.php?page=crowdsignal-forms-settings' ) ); ?>">
+			<?php
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Used for basic flow.
+			if ( ! empty( $_GET['settings-updated'] ) ) {
+				echo '<div class="updated fade crowdsignal-updated"><p>' . esc_html__( 'Settings successfully saved', 'crowdsignal-forms' ) . '</p></div>';
+			}
+
+			?>
+			<div id="settings-general" class="settings_panel">
+				<table class="form-table settings parent-settings">
+					<tr valign="top" class="">
+						<th scope="row"><label for="setting-crowdsignal_api_key"><?php esc_html_e( 'Enter Crowdsignal API Key', 'crowdsignal-forms' ); ?></a></th>
+						<td><input
+							<?php echo $api_key ? 'readonly' : ''; ?>
+							id="setting-crowdsignal_api_key"
+							class="regular-text"
+							type="text"
+							name="crowdsignal_api_key"
+							value="<?php echo esc_attr( $api_key ); ?>"
+							/>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="crowdsignal-settings__submit">
+			<?php
+			if ( $api_key ) {
+				wp_nonce_field( 'disconnect-api-key' );
+				?>
+				<input type="hidden" name="action" value="disconnect" />
+				<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Disconnect', 'crowdsignal-forms' ); ?>" />
+				<?php
+			} else {
+				wp_nonce_field( 'add-api-key' );
+				?>
+				<input type="hidden" name="action" value="update" />
+				<input type="submit" class="button-primary" value="<?php esc_attr_e( 'Connect', 'crowdsignal-forms' ); ?>" />
+				<?php
+			}
+			?>
+			</div>
+		</form></p>
+	</div>
+</div>
+
+<script>
+let CSCONNECT = null;
+const showConnect = ( title ) => {
+	// window size, match standard iPhone screen size
+	const widthRatio = 1/2;
+	const heightRatio = 3/4;
+
+	// Fixes dual-screen position
+	const dualScreenLeft = window.screenLeft !==  undefined
+		? window.screenLeft // Most browsers
+		: window.screenX; // Firefox
+	const dualScreenTop = window.screenTop !==  undefined
+		? window.screenTop // Most browsers
+		: window.screenY; // Firefox
+
+	const width = window.innerWidth
+		? window.innerWidth
+		: document.documentElement.clientWidth
+			? document.documentElement.clientWidth
+			: screen.width;
+	const height = window.innerHeight
+		? window.innerHeight
+		: document.documentElement.clientHeight
+			? document.documentElement.clientHeight
+			: screen.height;
+
+	const popupWidth = width * widthRatio;
+	const popupHeight = height * heightRatio;
+
+	const left = ( (width / 2 ) - ( popupWidth / 2 ) ) + dualScreenLeft;
+	const top = ( (height / 2 ) - ( popupHeight / 2 ) ) + dualScreenTop;
+
+	CSCONNECT = window.open( 'about:blank', title,
+		`
+		location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no,
+		width=${ parseInt( popupWidth, 10 ) },
+		height=${ parseInt( popupHeight, 10 )  },
+		top=${ parseInt( top, 10 ) },
+		left=${ parseInt( left, 10 ) }
+		`
+	)
+
+	if ( window.focus ) CSCONNECT.focus();
+}
+( function( form ) {
+	form.onsubmit = function() {
+		showConnect( 'CSCONNECT' );
+	}
+} )( document.getElementById( 'cs-connect-form' ) );
+</script>
