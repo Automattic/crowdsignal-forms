31eee2ed02f977e64e61c53769d949f17d37327f
Christian Gastrell <cgastrell@gmail.com>
2021-12-03 17:09:25 -0300

    Revert "Update the text and styling of the settings page. (#199)"
    This reverts commit dcdb04ea84d978e7c30a57a76bfd1f7f8c1afd16.

--- includes/admin/views/html-admin-dashboard-teaser.php
+++ includes/admin/views/html-admin-dashboard-teaser.php
@@ -13,55 +13,54 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class='jp-settings-container'>
 	<div class="dops-card dops-section-header is-compact">
 		<div class="dops-section-header__label">
-			<span class="dops-section-header__label-text"><?php esc_html_e( 'Manage your Crowdsignal projects inside WordPress', 'crowdsignal-forms' ); ?></span>
+			<span class="dops-section-header__label-text"><?php esc_html_e( 'Manage all your Crowdsignal projects and collected responses right in WP-Admin', 'crowdsignal-forms' ); ?></span>
 		</div>
 	</div>
 
 	<div class="dops-card dops-section-header is-compact">
 		<div class="jp-form-settings-group" style='width: 100%'>
-			<h2><?php echo wp_kses_post( __( 'The Crowdsignal Dashboard plugin', 'crowdsignal-forms' ) ); ?></h2>
-			<div class="crowdsignal-setup__middle">
-				<p>
-				<?php
-				printf(
-					/* translators: Placeholder is the text "second plugin". */
-					esc_html__( 'We have a %s for you that allows you to manage all your Crowdsignal projects right in WP-Admin. Get an overview of all your active projects and get easy access to your results pages.', 'crowdsignal-forms' ),
-					sprintf(
-						'<a href="https://wordpress.org/plugins/polldaddy/">%s</a>',
-						esc_html__( 'second plugin', 'crowdsignal-forms' )
-					)
-				);
-				?>
-				</p>
+			<h3><?php echo wp_kses_post( __( 'Install the Crowdsignal Dashboard plugin', 'crowdsignal-forms' ) ); ?></h3>
+		<div class="crowdsignal-setup__middle">
+			<p>
+				<?php echo wp_kses_post( __( 'This plugin will allow you to get the Crowdsignal dashboard without leaving WordPress.', 'crowdsignal-forms' ) ); ?>
+			</p>
 
-				<a href="<?php echo admin_url( 'plugin-install.php?s=crowdsignal+polls+ratings&tab=search&type=term' ); ?>"><img id='crowdsignal__teaser_img' src='<?php echo plugins_url( 'crowdsignal-forms/images/cs_dashboard_teaser.png' ); ?>' /></a>
-				<p>
-					<?php
-						echo wp_kses_post(
-							sprintf(
-								// translators: Argument is a link to Crowdsignal's contact page.
-								__(
-									'Do you want to know more about Crowdsignal? <a href="%1s" target="_blank">Learn more</a>.',
-									'crowdsignal-forms'
-								),
-								'https://crowdsignal.com/support/'
-							)
-						);
-						?>
-				</p>
-				<p>
-				<?php
-				printf(
-					/* translators: Placeholder is the text "website plugins page". */
-					esc_html__( 'Install the Crowdsignal Dashboard plugin directly from your %s.', 'crowdsignal-forms' ),
-					sprintf(
-						'<a href="' . admin_url( 'plugin-install.php?s=crowdsignal+polls+ratings&tab=search&type=term' ) . '">%s</a>',
-						esc_html__( 'website plugins page', 'crowdsignal-forms' )
-					)
-				);
-				?>
-				</p>
+			<div class="crowdsignal-setup__video-container">
+				<div class="crowdsignal-setup__video">
+					<iframe src="https://videopress.com/v/jWTs90Dg" frameborder="0" allowfullscreen></iframe>
+				</div>
 			</div>
+
+
+			<p>
+				<?php
+					echo wp_kses_post(
+						sprintf(
+							// translators: Argument is a link to Crowdsignal's contact page.
+							__(
+								'<a href="%1s" target="_blank">Any questions about Crowdsignal?</a>',
+								'crowdsignal-forms'
+							),
+							'https://crowdsignal.com/contact/'
+						)
+					);
+					?>
+			</p>
+
+			<p>
+				<?php
+					echo wp_kses_post(
+						sprintf(
+							// translators: Argument is a link to Crowdsignal's support page.
+							__(
+								'<a href="%1s" target="_blank">Read more about us here.</a>',
+								'crowdsignal-forms'
+							),
+							'https://crowdsignal.com/support/'
+						)
+					);
+					?>
+			</p>
 		</div>
 	</div>
 </div>
--- includes/admin/views/html-admin-setup-step-3.php
+++ includes/admin/views/html-admin-setup-step-3.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="dops-card dops-section-header is-compact">
 		<div class="jp-form-settings-group" style='width: 100%'>
-			<h2><?php echo wp_kses_post( __( 'First time using Crowdsignal?', 'crowdsignal-forms' ) ); ?></h2>
+			<h3><?php echo wp_kses_post( __( 'First time using Crowdsignal?', 'crowdsignal-forms' ) ); ?></h3>
 		<div class="crowdsignal-setup__middle">
 			<p>
 				<?php echo wp_kses_post( __( 'You can use Crowdsignal blocks right in your editor. Search for Crowdsignal in the blocks library and add the blocks to your page. Here is a short video to get you started:', 'crowdsignal-forms' ) ); ?>
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 			<div class="crowdsignal-setup__video-container">
 				<div class="crowdsignal-setup__video">
-					<iframe src="https://videopress.com/v/jWTs90Dg?autoplay=0&hd=1" frameborder="0" allowfullscreen></iframe>
+					<iframe src="https://videopress.com/v/jWTs90Dg" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 
@@ -36,9 +36,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 				<?php
 					echo wp_kses_post(
 						sprintf(
+							// translators: Argument is a link to Crowdsignal's contact page.
+							__(
+								'<a href="%1s" target="_blank">Any questions about Crowdsignal?</a>',
+								'crowdsignal-forms'
+							),
+							'https://crowdsignal.com/contact/'
+						)
+					);
+					?>
+			</p>
+
+			<p>
+				<?php
+					echo wp_kses_post(
+						sprintf(
 							// translators: Argument is a link to Crowdsignal's support page.
 							__(
-								'Do you want to know more about Crowdsignal and our blocks? <a href="%1s" target="_blank">Learn more</a>.',
+								'<a href="%1s" target="_blank">Read more about us here.</a>',
 								'crowdsignal-forms'
 							),
 							'https://crowdsignal.com/support/'
