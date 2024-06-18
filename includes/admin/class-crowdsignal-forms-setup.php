56824a75566cc4c7a0bc52b38cecf100c85950f2
John Caruso <johncaruso@hey.com>
2024-06-18 10:49:28 -0400

    release v1.7.2

--- includes/admin/class-crowdsignal-forms-setup.php
+++ includes/admin/class-crowdsignal-forms-setup.php
@@ -67,7 +67,7 @@ class Crowdsignal_Forms_Setup {
 	 * Enqueues scripts for setup page.
 	 */
 	public function admin_enqueue_scripts() {
-		wp_enqueue_style( 'admin-styles', plugin_dir_url( __FILE__ ) . '/admin-styles.css', array(), '1.7.1' );
+		wp_enqueue_style( 'admin-styles', plugin_dir_url( __FILE__ ) . '/admin-styles.css', array(), '1.7.2' );
 		wp_enqueue_script( 'videopress', 'https://videopress.com/videopress-iframe.js', array(), '1.0', false );
 	}
 
